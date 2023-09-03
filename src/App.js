4a4381e4c831fdaa371c837d376e738febd3dcb0
Maj Mohar <maj.mohar4@gmail.com>
2023-09-03 19:51:08 +0200

    popravljeno

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [showBanner, setShowBanner] = useState(false);
+  const [bannerMessage, setBannerMessage] = useState("");
+  const [additionalClassName, setAdditionalClassName] = useState("");
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/api/request`)
+      .then((response) => response.json())
+      .then((result) => {
+        if (result.sporočilo) {
+          setBannerMessage(result.sporočilo);
+          setShowBanner(true);
+          setTimeout(() => {
+            closeBanner();
+          }, 5000);
+        } else {
+          setShowBanner(false);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const closeBanner = () => {
+    setAdditionalClassName("closing");
+
+    setTimeout(() => {
+      setShowBanner(false);
+    }, 2000);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {showBanner && (
+        <div className={`banner ${additionalClassName} app`}>
+          <span className="text">{bannerMessage}</span>
+          <button className="close-button" onClick={closeBanner}>
+            X
+          </button>
+        </div>
+      )}
+      <h1>React aplikacija</h1>
     </div>
   );
 }
